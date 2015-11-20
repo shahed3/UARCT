163299c3cd250e19008536a8799b1e1d24ed8882
syed shahied mehdi <syed_smehdi@yahoo.com>
2015-11-20 12:53:02 +0530

    uarct education
    uarct education project

---
+++ UARCT_EDU/Source/UARCT.EDU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UARCT.EDU.MvcApplication" Language="C#" %>
---
+++ UARCT_EDU/Source/UARCT.EDU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UARCT.EDU.Startup))]
+namespace UARCT.EDU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
